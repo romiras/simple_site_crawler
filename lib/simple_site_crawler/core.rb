711135273ed980c3c9ac56cdeb6d2b7d8975a548
Romiras <298544+romiras@users.noreply.github.com>
2022-10-12 22:42:16 +0300

    WIP

---
+++ lib/simple_site_crawler/core.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module SimpleSiteCrawler
+  # Core class of crawler
+  class Core
+    def initialize(base_url)
+      @base_url = base_url
+    end
+
+    def call
+      # TODO
+    end
+  end
+end
